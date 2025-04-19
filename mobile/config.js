2f82e6f1702bd43d0563817718b701ee47c87f9c
I was told to put my name here, but I didn't want to <127406648+LegitKnots@users.noreply.github.com>
2025-04-19 04:22:44 -0400

    Moved backend up a dir

--- mobile/config.js
+++ mobile/config.js
@@ -1,2 +1,1 @@
-// src/config.js (or config.ts if you're using TS)
-export const SERVER_URI = "http://192.168.4.26:3001"; // or whatever your backend is
+export const SERVER_URI = "http://10.0.1.16:3001";
