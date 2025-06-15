9a1f78a44601bfe4af4d37dfdf4196e03ef2ba08
I was told to put my name here, but I didn't want to <127406648+LegitKnots@users.noreply.github.com>
2025-06-15 17:35:52 -0400

    Changed to Prod settings

--- mobile/config.js
+++ mobile/config.js
@@ -1,3 +1,3 @@
-export const SERVER_URI = "https://pc.notaburg.com";
+export const SERVER_URI = "https://packtrack.notaburg.com";
 export const GOOGLE_MAPS_APIKEY = "AIzaSyALOLeklnmLqvYB3Sn4n_TQeHLn2pr71Ts";
 export const PRIMARY_APP_COLOR = "#f3631a"
