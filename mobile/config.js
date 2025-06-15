b89bec75343c9d9d42c1c7b46f7f20dad0aecbae
I was told to put my name here, but I didn't want to <127406648+LegitKnots@users.noreply.github.com>
2025-06-15 02:00:46 -0400

    Update config.js

--- mobile/config.js
+++ mobile/config.js
@@ -1,3 +1,3 @@
-export const SERVER_URI = "http://10.0.1.16:3001";
+export const SERVER_URI = "https://pc.notaburg.com";
 export const GOOGLE_MAPS_APIKEY = "AIzaSyALOLeklnmLqvYB3Sn4n_TQeHLn2pr71Ts";
 export const PRIMARY_APP_COLOR = "#f3631a"
